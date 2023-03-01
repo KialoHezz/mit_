ea46d2267f762234372f2a4f94c952cb029f10d8
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-03-01 12:33:11 +0300

    Interacted with te irb at rails console and root url

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,12 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_01_085047) do
   create_table "students", force: :cascade do |t|
+    # there will be ID which will auto incrementing
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+
   end
 
 end
